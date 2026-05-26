e0f6de82e33058c921e9d365b1933613245565cf
jaisudharshan18 <jaisudharshanjaisudharshan.com>
2026-05-26 17:33:52 +0530

    UI changed3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Component, type ReactNode, type ErrorInfo } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
